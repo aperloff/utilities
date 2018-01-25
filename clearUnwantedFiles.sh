cebef9b022dee8db656de168df4b1db4ee1c272b
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-01-25 11:31:05 -0600

    Fix bug in clearUnwantedFiles.sh due to missing location for find.

--- clearUnwantedFiles.sh
+++ clearUnwantedFiles.sh
@@ -1,20 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 export DEPTH=1
 while getopts d option
 do
-	case "${option}"
-	in
-	d) DEPTH=${OPTARG};;
-	esac
+    case "${option}"
+    in
+    d) DEPTH=${OPTARG};;
+    esac
 done
+
+if [ -z "$1" ]; then
+    LOCATION=$PWD
+else
+    LOCATION=${1}
+fi
 
 declare -a types=( "txt" "C" "hh" "cc" "py" "sh" "csh" "cfg" "xml" "jdl" "pbs" )
 for type in "${types[@]}" ; do
-	find $1 -type f -name "*.${type}~" -exec rm -f '{}' ';'
+    find "${LOCATION}" -type f -name "*.${type}~" -exec rm -f '{}' ';'
 done
 
 declare -a patterns=( ".pbs.o" ".pbs.e" )
 for pattern in "${patterns[@]}" ; do
-	find $1 -type f -name "*${pattern}*" -exec rm -f '{}' ';'
+    find "${LOCATION}" -type f -name "*${pattern}*" -exec rm -f '{}' ';'
 done
