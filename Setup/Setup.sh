2d6158e2b05c753b46bad2f5447defbb5f492010
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-02-15 08:48:30 -0600

    Minor updates to the setup scripts.

--- Setup/Setup.sh
+++ Setup/Setup.sh
@@ -71,7 +71,7 @@ select proj in "${PROJECTS[@]}"; do
           break
           ;;
         "CMSDAS",*|*,"CMSDAS")
-          PROJECT_PATHS=("CMSDAS/2017/" "CMSDAS/2018/" "CMSDAS/2019/")
+          PROJECT_PATHS=("CMSDAS/2017/" "CMSDAS/2018/" "CMSDAS/2019/" "CMSDAS/2020/")
           break
           ;;
         "HATS@LPC",*|*,"HATS@LPC")
