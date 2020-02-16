b364d3e7570ad722671b606a60bfc82e03a9eccc
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2020-02-16 17:29:06 -0600

    Add the ability to change the units used to report the tree information and the per event branch information, separately.

--- ROOT/analyzeTFileSizes.py
+++ ROOT/analyzeTFileSizes.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 from __future__ import print_function
+from enum import Enum
 import argparse,os,pkgutil,sys
 try:
     import uproot
 except ImportError:
     raise ImportError("Could not find the module uproot. Check that it is installed on your system.")
+
+class Unit(Enum):
+    Byte = 1024**0
+    KiB  = 1024**1
+    MiB  = 1024**2
+    GiB  = 1024**3
+    TiB  = 1024**4
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def from_string(s):
+        try:
+            return Unit[s]
+        except KeyError:
+            raise ValueError()
 
 class TTreeInfo:
     def __init__(self, nentries = 0, nbranches=0, sum_of_branch_sizes=0):
@@ -12,22 +30,19 @@ class TTreeInfo:
         self.nbranches = nbranches
         self.sum_of_branch_sizes = sum_of_branch_sizes
 
-    def get_size_kb(self):
-        return self.sum_of_branch_sizes/1024.
+    def get_size(self, unit=Unit.Byte):
+        return self.sum_of_branch_sizes/float(unit.value)
 
-    def get_size_per_event(self):
-        return self.sum_of_branch_sizes/self.nentries
-
-    def get_size_per_event_kb(self):
-        return self.sum_of_branch_sizes/1024./self.nentries
+    def get_size_per_event(self, unit=Unit.Byte):
+        return self.sum_of_branch_sizes/self.nentries/float(unit.value)
 
 class TBranchInfo:
     def __init__(self, name, size=0):
         self.name = name
         self.size = size
 
-    def get_size_per_event(self, nentries):
-        return self.size/float(nentries)
+    def get_size_per_event(self, nentries, unit=Unit.Byte):
+        return self.size/float(nentries)/float(unit.value)
 
     def get_fraction(self, den):
         return 100.0*self.size/float(den)
@@ -37,8 +52,8 @@ class GroupInfo:
         self.name = name
         self.size = 0
 
-    def get_size_per_event(self, nentries):
-        return self.size/float(nentries)
+    def get_size_per_event(self, nentries, unit=Unit.Byte):
+        return self.size/float(nentries)/float(unit.value)
 
     def get_fraction(self, den):
         return 100.0*self.size/float(den)
@@ -69,7 +84,7 @@ def running_total_size(list,end_index):
 def running_total_fraction(list,end_index,den):
     return 100.00*running_total_size(list,end_index)/float(den)
 
-def analyzeTFileSizes(filename, debug=False, sort=False, tbranch="", tdirectory="", ttree="", uncompressed=False):
+def analyzeTFileSizes(filename, branch_unit=Unit.Byte, debug=False, sort=False, tbranch="", tdirectory="", ttree="", uncompressed=False, tree_unit=Unit.Byte):
     if not run_checks(filename):
         raise Exception("Unable to pass the basic sanity checks!")
 
@@ -116,20 +131,21 @@ def analyzeTFileSizes(filename, debug=False, sort=False, tbranch="", tdirectory=
     print("\nFile: "+filename)
     print("Tree: {0}".format(treename))
     print("Entries: {0:,d}".format(ttreeinfo.nentries))
-    print("Size: {0:0.2f} kB ({1})".format(ttreeinfo.get_size_kb(),"uncompressed" if uncompressed else "compressed"))
-    print("Size/Entry: {0:0.2f} kB ({1})\n".format(ttreeinfo.get_size_per_event_kb(),"uncompressed" if uncompressed else "compressed"))
-    print(fmt_header.format("Branch name",max_length,"Byte/ev",wbytes,"Frac. [%]",wother,"Cumulative",wother," "*6,"Branch group name",max_length,"Byte/ev",wbytes,"Frac. [%]",wother,"Cumulative",wother))
+    print("Size: {0:0.2f} {1} ({2})".format(ttreeinfo.get_size(tree_unit),tree_unit.name,"uncompressed" if uncompressed else "compressed"))
+    print("Size/Entry: {0:0.2f} {1} ({2})\n".format(ttreeinfo.get_size_per_event(tree_unit),tree_unit.name,"uncompressed" if uncompressed else "compressed"))
+    print(fmt_header.format("Branch name",max_length,branch_unit.name+"/ev",wbytes,"Frac. [%]",wother,"Cumulative",wother," "*6, \
+                            "Branch group name",max_length,branch_unit.name+"/ev",wbytes,"Frac. [%]",wother,"Cumulative",wother))
     print("="*(max_length+wbytes+2*wother+3)+" "*6+"="*(max_length+wbytes+2*wother+3))
-    print(fmt_total.format("Total",max_length,ttreeinfo.get_size_per_event(),wbytes,"100.00",wother,"-",wother) \
+    print(fmt_total.format("Total",max_length,ttreeinfo.get_size_per_event(branch_unit),wbytes,"100.00",wother,"-",wother) \
           + " "*6 + \
-          fmt_total.format("Total",max_length,ttreeinfo.get_size_per_event(),wbytes,"100.00",wother,"-",wother))
+          fmt_total.format("Total",max_length,ttreeinfo.get_size_per_event(branch_unit),wbytes,"100.00",wother,"-",wother))
     for itbi, tbi in enumerate(tbranchinfo_list):
-        s  = fmt.format(tbi.name,max_length,tbi.get_size_per_event(ttreeinfo.nentries),wbytes, \
+        s  = fmt.format(tbi.name,max_length,tbi.get_size_per_event(ttreeinfo.nentries,branch_unit),wbytes, \
                         tbi.get_fraction(ttreeinfo.sum_of_branch_sizes),wother, \
                         running_total_fraction(tbranchinfo_list,itbi+1,ttreeinfo.sum_of_branch_sizes),wother)
         s += " "*6
         if itbi < len (groupinfo_list):
-            s += fmt.format(groupinfo_list[itbi].name,max_length,groupinfo_list[itbi].get_size_per_event(ttreeinfo.nentries),wbytes, \
+            s += fmt.format(groupinfo_list[itbi].name,max_length,groupinfo_list[itbi].get_size_per_event(ttreeinfo.nentries,branch_unit),wbytes, \
                             groupinfo_list[itbi].get_fraction(ttreeinfo.sum_of_branch_sizes),wother, \
                             running_total_fraction(groupinfo_list,itbi+1,ttreeinfo.sum_of_branch_sizes),wother)
         print(s)
@@ -141,12 +157,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Return the average size of an event from a ROOT file",
                                      epilog="And those are the options available. Deal with it.")
     parser.add_argument("filename", help="The full path and name of the ROOT file to analyze")
+    parser.add_argument("--branch_unit", default=Unit.Byte, type=Unit.from_string, choices=list(Unit), help="The base unit used to return the per event size of a branch (default=%(default)s)")
     parser.add_argument("-d","--debug", action="store_true", help="Shows some extra information in order to debug this program (default=%(default)s)")
     parser.add_argument("-s","--sort", default=False, action="store_true", help="Short the branches and groups by size rather than by name (default=%(default)s)")
     parser.add_argument("--tbranch", default="", help="The name of a specific TBranch to study (default=%(default)s)")
     parser.add_argument("--tdirectory", default="TreeMaker2", help="The TDirectory name within the TFile containing the TTree (default=%(default)s)")
     parser.add_argument("--ttree", default="PreSelection", help="The TTree name within the chosen TFile and TDirectory (default=%(default)s)")
     parser.add_argument("-u","--uncompressed", default=False, action="store_true", help="Return the uncompressed sizes rather than the compressed sizes (default=%(default)s)")
+    parser.add_argument("--tree_unit", default=Unit.KiB, type=Unit.from_string, choices=list(Unit), help="The base unit used to return the size of the tree (default=%(default)s)")
     parser.add_argument('--version', action='version', version='%(prog)s v1.0')
     args = parser.parse_args()
 
